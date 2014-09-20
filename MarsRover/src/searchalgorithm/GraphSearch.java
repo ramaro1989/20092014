53e822fccee3a360c45780cbc13778fab30fb927
ramaro <ra.amaro@campus.fct.unl.pt>
2014-09-20 20:48:30 +0100

    Graph v1

--- MarsRover/src/searchalgorithm/GraphSearch.java
+++ MarsRover/src/searchalgorithm/GraphSearch.java
@@ -1,20 +1,66 @@
 package searchalgorithm;
 
 import java.util.*;
+
 import searchproblem.*;
 
 public class GraphSearch implements SearchAlgorithm {
-
+	private boolean done = false;
+	private SearchProblem problem;
+	private Node goal;
+	private Queue<Node> frontier;
+	private int expansions;
+	private int generated;
+	private HashMap<Node,Node> explored;
+	
 	public GraphSearch(SearchProblem p, Queue<Node> q) {
-
+		explored = new HashMap<Node, Node> ();
+		problem = p;
+		goal = null;
+		frontier = q;
+		expansions = 0;
+		generated = 0;
+		
 	}
 	
 	public Node searchSolution() {
-
+		if( !done ) {
+			goal = search();
+			done = true;
+			frontier = null;
+			problem = null;
+		}
+		return goal;
 	}
 
-	public Map<String,Number> getMetrics() {
-
+	private Node search() {		
+		frontier.clear();
+		frontier.add(new Node(problem.getInitial()));
+		generated++;
+		for(;;) {
+			if( frontier.isEmpty() ) {
+				return null;
+			}
+			Node n = frontier.remove();
+			if( problem.goalTest(n.getState())) {
+				return n;
+			}
+			if(!explored.containsValue(n)){
+				explored.put(n, n);
+				expansions++;
+				List<Node> children = n.Expand();
+				generated += children.size();
+				frontier.addAll(children);
+			}
+			
+		}
 	}
 	
+	public Map<String,Number> getMetrics() {
+		Map<String,Number> metrics = new LinkedHashMap<String,Number>();
+		
+		metrics.put("Node Expansions",expansions);
+		metrics.put("Nodes Generated",generated);
+		return metrics;
+	}
 }
