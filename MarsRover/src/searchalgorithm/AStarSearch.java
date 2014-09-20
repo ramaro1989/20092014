81135c6a02f83baef3e3b29bfe8b14a449b74a82
soares <pedro.soares@javali.com>
2014-09-20 18:59:14 +0100

    Astar v2

--- MarsRover/src/searchalgorithm/AStarSearch.java
+++ MarsRover/src/searchalgorithm/AStarSearch.java
@@ -1,6 +1,9 @@
 package searchalgorithm;
 
-import java.util.*;
+import java.util.Comparator;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 import searchproblem.InformedSearchProblem;
 
@@ -8,22 +11,27 @@ public class AStarSearch implements SearchAlgorithm {
 	private GraphSearch graph;
 
 	public AStarSearch(final InformedSearchProblem p) {
-		Queue<Node> pqueue = new PriorityQueue<Node>(11,
-				new Comparator<Node>() {
 
-					public int compare(Node o1, Node o2) {
+		Comparator<Node> compare = new Comparator<Node>() {
 
-						double cost1 = o1.getPathCost() + p.heuristic(o1);
-						double cost2 = o2.getPathCost() + p.heuristic(o2);
+			@Override
+			public int compare(Node o1, Node o2) {
+				// TODO Auto-generated method stub
+				double cost1 = o1.getPathCost() + p.heuristic(o1);
+				double cost2 = o2.getPathCost() + p.heuristic(o2);
 
-						if (cost1 > cost2)
-							return 1;
-						else if (cost1 < cost2)
-							return -1;
-						else
-							return 0;
-					}
-				});
+				if (cost1 > cost2)
+					return 1;
+				else if (cost1 < cost2)
+					return -1;
+				else
+					return 0;
+			}
+
+		};
+		
+		Queue<Node> pqueue = new PriorityQueue<Node>(20, compare);
+		
 		graph = new GraphSearch(p, pqueue);
 
 	}
