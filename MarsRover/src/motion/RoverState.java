d687fdef5b61574c1ae58330216d9c07cbe9c8f0
soares <pedro.soares@javali.com>
2014-09-23 16:29:41 +0100

    alterei so uma cena no state para ficar igual ao metodo que chamei no problem

--- MarsRover/src/motion/RoverState.java
+++ MarsRover/src/motion/RoverState.java
@@ -7,32 +7,33 @@ import motion.Terrain.TerrainType;
 import static java.lang.Math.*;
 import searchproblem.*;
 
-
 public class RoverState extends State {
 
-	private int x,y,height;
+	private int x, y, height;
 	private BitmapTerrain map;
+
 	private enum RoverDirections {
 		N, S, E, W, NE, NW, SE, SW;
 	};
-	
+
 	public RoverState(int x, int y, BitmapTerrain map) {
-		this.x=x;
-		this.y=y;
-		this.map=map;
+		this.x = x;
+		this.y = y;
+		this.map = map;
 	}
+
 	public int getCoordX() {
 		return x;
 	}
+
 	public int getCoordY() {
 		return y;
 	}
-	
-	public int getheight() {
+
+	public int getHeight() {
 		return height;
 	}
-	
-	
+
 	@Override
 	public List<Arc> successorFunction() {
 		// TODO Auto-generated method stub
@@ -69,21 +70,20 @@ public class RoverState extends State {
 		return false;
 	}
 
-	private double getMoveCost(int x, int y){
-		int h=map.getHeight(x, y);
-		double dh = h-height;
+	private double getMoveCost(int x, int y) {
+		int h = map.getHeight(x, y);
+		double dh = h - height;
 		double dx = x - this.x * 10;
 		double dy = y - this.y * 10;
 		double distance = Math.sqrt(dx * dx + dy * dy + h * h);
 		double dhcost = Math.exp(Math.abs(dh));
-		double tType= 1.0;
-		
-		if(map.getTerrainType(x, y).equals(TerrainType.SAND))
-			tType=2.0;
-		if(map.getTerrainType(x, y).equals(TerrainType.ROCK))
-			tType=3.0;
-		
-		return distance * dhcost * tType; 
+		double tType = 1.0;
+
+		if (map.getTerrainType(x, y).equals(TerrainType.SAND))
+			tType = 2.0;
+		if (map.getTerrainType(x, y).equals(TerrainType.ROCK))
+			tType = 3.0;
+
+		return distance * dhcost * tType;
 	}
 }
-
