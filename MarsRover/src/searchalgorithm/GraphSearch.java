42609fabea9c542801a11bab6a8024a6a5b0d048
soares <pedro.soares@javali.com>
2014-10-12 16:21:48 +0100

    graphsearch

--- MarsRover/src/searchalgorithm/GraphSearch.java
+++ MarsRover/src/searchalgorithm/GraphSearch.java
@@ -11,20 +11,20 @@ public class GraphSearch implements SearchAlgorithm {
 	private Queue<Node> frontier;
 	private int expansions;
 	private int generated;
-	private HashMap<Node,Node> explored;
-	
+	private HashMap<Node, Node> explored;
+
 	public GraphSearch(SearchProblem p, Queue<Node> q) {
-		explored = new HashMap<Node, Node> ();
+		explored = new HashMap<Node, Node>();
 		problem = p;
 		goal = null;
 		frontier = q;
 		expansions = 0;
 		generated = 0;
-		
+
 	}
-	
+
 	public Node searchSolution() {
-		if( !done ) {
+		if (!done) {
 			goal = search();
 			done = true;
 			frontier = null;
@@ -33,34 +33,44 @@ public class GraphSearch implements SearchAlgorithm {
 		return goal;
 	}
 
-	private Node search() {		
+	private Node search() {
 		frontier.clear();
 		frontier.add(new Node(problem.getInitial()));
 		generated++;
-		for(;;) {
-			if( frontier.isEmpty() ) {
+		for (;;) {
+			if (frontier.isEmpty()) {
 				return null;
 			}
 			Node n = frontier.remove();
-			if( problem.goalTest(n.getState())) {
+			if (problem.goalTest(n.getState())) {
 				return n;
 			}
-			if(!explored.containsValue(n)){
+			if (!explored.containsValue(n)) {
 				explored.put(n, n);
 				expansions++;
 				List<Node> children = n.Expand();
 				generated += children.size();
 				frontier.addAll(children);
+			} else {
+
+				Node old = explored.get(n);
+				if (old.getPathCost() > n.getPathCost()) {
+					explored.put(n, n);
+					List<Node> children = n.Expand();
+					expansions++;
+					frontier.addAll(children);
+					generated += children.size();
+				}
+
 			}
-			
 		}
 	}
-	
-	public Map<String,Number> getMetrics() {
-		Map<String,Number> metrics = new LinkedHashMap<String,Number>();
-		
-		metrics.put("Node Expansions",expansions);
-		metrics.put("Nodes Generated",generated);
+
+	public Map<String, Number> getMetrics() {
+		Map<String, Number> metrics = new LinkedHashMap<String, Number>();
+
+		metrics.put("Node Expansions", expansions);
+		metrics.put("Nodes Generated", generated);
 		return metrics;
 	}
 }
